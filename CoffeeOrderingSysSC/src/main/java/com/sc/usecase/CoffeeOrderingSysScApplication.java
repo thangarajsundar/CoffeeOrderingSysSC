bdf9771435120f5bd95eff9d283613b89b70f196
thang <thang@192.168.2.5>
2018-08-06 23:37:33 +0530

    First commit for CoffeeOrderingSysSC

---
+++ CoffeeOrderingSysSC/src/main/java/com/sc/usecase/CoffeeOrderingSysScApplication.java
@@ -0,0 +1,12 @@
+package com.sc.usecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderingSysScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeOrderingSysScApplication.class, args);
+	}
+}
